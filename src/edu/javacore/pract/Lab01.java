0f8e667b386625005a4967427c9ac4b4e86e0d0d
Raushanray <raushankuray1998@gmail.com>
2023-04-04 09:32:55 +0530

    update

--- src/edu/javacore/pract/Lab01.java
+++ src/edu/javacore/pract/Lab01.java
@@ -4,13 +4,32 @@ public class Lab01 {
 
 	public static void main(String[] args) {
 		Hello h1 = new Hello();
-		int aValue = h1.a;
-		System.out.println("Value of a is " + aValue);
+		//int aValue = h1.a;
+		h1.show();
+		//System.out.println("\"" + "Value" + "\"");
 	}
 
 }
 
 class Hello {
 	int a;
+	short s;
+	byte b;
+	boolean b1;
+	long l;
+	double d;
+	float f;
+	String str;
 
+	void show() {
+		System.out.println("Value of a is " + a);
+		System.out.println("Value of s is " + s);
+		System.out.println("Value of b is " + b);
+		System.out.println("Value of b1 is " + b1);
+		System.out.println("Value of l is " + l);
+		System.out.println("Value of d is " + d);
+		System.out.println("Value of f is " + f);
+		System.out.println("Value of str is " + str);
+	}
+    
 }
