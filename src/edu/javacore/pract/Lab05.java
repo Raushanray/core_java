839d6db65bb44548ae724444e7797d906c05ccf9
Raushanray <raushankuray1998@gmail.com>
2023-04-05 18:53:23 +0530

    Lab05 example of static variables

--- src/edu/javacore/pract/Lab05.java
+++ src/edu/javacore/pract/Lab05.java
@@ -1,8 +1,9 @@
 package edu.javacore.pract;
 
 public class Lab05 {
-static int a;
-public static void main(String[] args) {
-	System.out.println(a);
-}
+	static int a;
+
+	public static void main(String[] args) {
+		System.out.println(a);
+	}
 }
