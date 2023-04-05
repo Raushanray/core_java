7a13e892e8c24805be6a561172880cb57e6fe688
Raushanray <raushankuray1998@gmail.com>
2023-04-05 18:56:37 +0530

    Lab06 a used but not initialized

--- src/edu/javacore/pract/Lab06.java
+++ src/edu/javacore/pract/Lab06.java
@@ -1,9 +1,10 @@
 package edu.javacore.pract;
 
 public class Lab06 {
- int a;
- public static void main(String[] args) {
-	 System.out.println(a);
-}
- 
+	int a;
+
+	public static void main(String[] args) {
+		System.out.println(a);
+	}
+
 }
