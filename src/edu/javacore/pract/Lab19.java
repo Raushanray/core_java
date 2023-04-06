4ac19232981f8812b3a71c80e3e1e26582b07799
Raushanray <raushankuray1998@gmail.com>
2023-04-06 08:12:51 +0530

    Lab19 example of single variable

--- src/edu/javacore/pract/Lab19.java
+++ src/edu/javacore/pract/Lab19.java
@@ -1,8 +1,8 @@
 package edu.javacore.pract;
 
 public class Lab19 {
-public static void main(String[] args) {
-	int a = 99;
-	System.out.println(a);
-}
+	public static void main(String[] args) {
+		int a = 99;
+		System.out.println(a);
+	}
 }
