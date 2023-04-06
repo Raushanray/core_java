f78652cd8e2a79ca308ee701200d182f41bab3c0
Raushanray <raushankuray1998@gmail.com>
2023-04-06 07:56:00 +0530

    Lab08 Example of initializing variables

--- src/edu/javacore/pract/Lab08.java
+++ src/edu/javacore/pract/Lab08.java
@@ -3,9 +3,8 @@ package edu.javacore.pract;
 public class Lab08 {
 
 	public static void main(String[] args) {
-    int a;
-    a = 99;
-    System.out.println("value of a is " + a);
+		int a;
+		a = 99;
+		System.out.println("value of a is " + a);
 	}
-
 }
